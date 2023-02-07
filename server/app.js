423fc6fca5b998eda7d644ca9fab7065cf5aed01
yoni2528 <atiasyehonatan@gmail.com>
2023-02-07 15:57:47 +0200

    Aws Deployment

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(xss());
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/test", (req, res) => {
-  res.send("welcome to my server this is test number 9");
+  res.send("welcome to my server this is test number 10");
 });
 app.use("/app/v1/users", userRouter);
 app.use("/app/v1/tokens", tokenRouter);
